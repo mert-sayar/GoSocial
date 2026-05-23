917a4be2e3d9aa8f8d40dbd1c9d43373a4345045
Mert <42028387+mert-sayar@users.noreply.github.com>
2026-05-24 00:16:09 +0300

    initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+
+	"github.com/mert-sayar/GoSocial/internal/db"
+	"github.com/mert-sayar/GoSocial/internal/env"
+	"github.com/mert-sayar/GoSocial/internal/store"
+)
+
+const version = "0.0.1"
+
+func main() {
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgresql://admin:adminpassword@localhost/gosocial?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
+
+		env: env.GetString("ENV", "development"),
+	}
+
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Print("database connection pool established")
+
+	store := store.NewStorage(db)
+
+	app := &application{
+		config: cfg,
+		store:  store,
+	}
+
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
+}
