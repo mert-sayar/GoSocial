08c1952ef2e99b9c9aeb2d6ac3233db06caebf23
Mert <42028387+mert-sayar@users.noreply.github.com>
2026-09-13 05:35:31 +0300

    replace standart logging with uber-go zap structured logger

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"time"
 
@@ -11,11 +10,13 @@ import (
 	"github.com/mert-sayar/GoSocial/docs" // This is required to generate swagger docs
 	"github.com/mert-sayar/GoSocial/internal/store"
 	httpSwagger "github.com/swaggo/http-swagger"
+	"go.uber.org/zap"
 )
 
 type application struct {
 	config config
 	store  store.Storage
+	logger *zap.SugaredLogger
 }
 
 type config struct {
@@ -93,7 +94,7 @@ func (app *application) run(mux http.Handler) error {
 		IdleTimeout:  time.Minute,
 	}
 
-	log.Printf("server has started at %s", app.config.addr)
+	app.logger.Infow("server has started at", "addr", app.config.addr, "env", app.config.env)
 
 	return srv.ListenAndServe()
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"log"
-
 	"github.com/mert-sayar/GoSocial/internal/db"
 	"github.com/mert-sayar/GoSocial/internal/env"
 	"github.com/mert-sayar/GoSocial/internal/store"
+	"go.uber.org/zap"
 )
 
 const version = "0.0.1"
@@ -41,6 +40,11 @@ func main() {
 		env: env.GetString("ENV", "development"),
 	}
 
+	// Logger
+	logger := zap.Must(zap.NewProduction()).Sugar()
+	defer logger.Sync()
+
+	// Database
 	db, err := db.New(
 		cfg.db.addr,
 		cfg.db.maxOpenConns,
@@ -48,20 +52,21 @@ func main() {
 		cfg.db.maxIdleTime,
 	)
 	if err != nil {
-		log.Panic(err)
+		logger.Fatal(err)
 	}
 
 	defer db.Close()
-	log.Print("database connection pool established")
+	logger.Info("database connection pool established")
 
 	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
 		store:  store,
+		logger: logger,
 	}
 
 	mux := app.mount()
 
-	log.Fatal(app.run(mux))
+	logger.Fatal(app.run(mux))
 }
